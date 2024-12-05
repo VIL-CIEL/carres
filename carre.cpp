8a385ba53ab9e291161732a6821279c3f1d1d5f1
Fabien Villedieu <fabien.villedieu.pro@gmail.com>
2024-12-05 09:16:40 +0100

    #2 class CCarre

---
+++ carre.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "carre.h"
+using namespace std;
+
+int main() {
+
+
+	return 0;
+}
--- carre.h
+++ carre.h
@@ -1,1 +1,16 @@
 #pragma once
+#include <iostream>
+using namespace std;
+
+class CCarre
+{
+public:
+	CCarre();
+
+private:
+	int sx, sy, cote;
+};
+
+CCarre::CCarre()
+{
+}
